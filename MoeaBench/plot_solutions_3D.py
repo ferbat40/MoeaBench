9e9fd083a7f55d71e40d15324fe073ae1bb40008
Fernando <setec.solda@gmail.com>
2025-10-03 13:52:56 -0300

    dsf

--- MoeaBench/plot_solutions_3D.py
+++ MoeaBench/plot_solutions_3D.py
@@ -35,53 +35,7 @@ class plot_solutions_3D:
          for idx_gen, gen in enumerate(self.vet_pts, start = self.generations[0]):
               for idx_moea, pts in enumerate(gen, start = 0):
                  print(idx_gen,pts,self.experiments[idx_moea])
-                 ax = pts[:,0]
-                 ay = pts[:,1]
-                 az = pts[:,2]
-                 self.figure.add_trace(go.Scatter3d(
-                 x=ax, y=ay, z=az,
-                 mode='markers',
-                 marker=dict(size=3),  
-                 name=f'{self.experiments[idx_moea]}',                       
-                 showlegend=True,
-                 hovertemplate = (f"gen {idx_gen}<br>"
-                                  f"Moea {self.experiments[idx_moea]}<br>"
-                                  f"X: %{{x}}<br>"
-                                  f"Y: %{{y}}<br>"
-                                  f"Z: %{{z}}<br><extra></extra>"),
-                 ))   
-       
-      
-         self.figure.update_layout(
-                scene = dict(
-                    xaxis=dict(title='axis X', showbackground=True, backgroundcolor="aliceblue", showgrid=True, gridcolor="#C3BDBD"),
-                    yaxis=dict(title='axis Y', showbackground=True, backgroundcolor="aliceblue", showgrid=True, gridcolor="#C3BDBD"),
-                    zaxis=dict(title='axis Z', showbackground=True, backgroundcolor="aliceblue", showgrid=True, gridcolor="#C3BDBD"),
-                    aspectmode='manual',
-                    aspectratio=dict(x=1,y=1,z=1)
-                 ),
                  
-                 width=900,
-                 height=800,
-                 margin=dict(l=0,r=0,b=0,t=0),
-                 title=dict(
-                     text=f'3D Solution Distribution Chart',
-                     x=0.5,
-                     xanchor='center',
-                     y=0.9,
-                     yanchor='bottom',
-                     pad=dict(t=0),
-                     font=dict(size=16)
-                 ),
-                 legend=dict(
-                     x=1,
-                     y=0.5,
-                     xanchor='left',
-                     yanchor='middle'
-               )
-            )
-         display(self.figure)
-
 
 
     

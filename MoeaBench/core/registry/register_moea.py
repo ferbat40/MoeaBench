432243ef4881efb733062cec99f2eda886604f98
Fernando <setec.solda@gmail.com>
2025-10-09 16:29:51 -0300

    dfsf

--- MoeaBench/core/registry/register_moea.py
+++ MoeaBench/core/registry/register_moea.py
@@ -1,11 +1,14 @@
 
-from MoeaBench.core.utils.save_moea import save_moea
 
 M_register = {}
 
 def register_moea(name):
     def decorator(cls):
         M_register[name] = cls
-        save_moea(cls)
+
         return cls
     return decorator
+
+
+def get_moea(name):
+    return M_register.get(name)

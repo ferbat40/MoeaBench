903a199df77a4ca7e9f37394d9d71d943238effb
Fernando <setec.solda@gmail.com>
2025-10-11 23:46:33 -0300

    sffsd

--- MoeaBench/DTLZ2.py
+++ MoeaBench/DTLZ2.py
@@ -58,6 +58,7 @@ class DTLZ2(H_DTLZ):
         result =  {"F" : F} 
         if n_ieq != 0:       
             result["G"] = self.constraints(F)
+            result["feasible"] >= np.all(G >=0)
         return result
        
 

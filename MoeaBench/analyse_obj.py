8c7295c7e9c6d174a8e91592c92e4390ce58647b
Fernando <setec.solda@gmail.com>
2025-10-13 22:12:23 -0300

    df

--- MoeaBench/analyse_obj.py
+++ MoeaBench/analyse_obj.py
@@ -6,14 +6,14 @@ class analyse_obj(plot_3D):
         
     @staticmethod
     def IPL_plot_3D(experiments, data, bench, array, objectives):
-        try:
+        #try:
             analyse_obj.allowed_obj(bench,bench[0],experiments,objectives)
             axis =  [i for i in range(0,3)]    if len(objectives) == 0 else [i-1 if i > 0 else 0 for i in objectives] 
             if not len([i for i in array if len(i) == 0]) == 0:   
                 raise ValueError (f'No results found for plot')
             plot_3D_obj =  analyse_obj(bench,array,experiments,axis)
             plot_3D_obj.configure()
-        except Exception as e:
-            print(e)
+        #except Exception as e:
+           # print(e)
 
     

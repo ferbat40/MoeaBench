060536b6beeefacdf0c154527057da0b5a4200b1
Fernando <setec.solda@gmail.com>
2025-10-16 11:55:16 -0300

    sdfs

--- MoeaBench/MoeaBench.py
+++ MoeaBench/MoeaBench.py
@@ -103,7 +103,7 @@ class MoeaBench(I_UserMoeaBench):
         
         
     def run(self):
-        #try:
+        try:
             name_moea=None
             name_benchmark=None
             try:
@@ -119,8 +119,8 @@ class MoeaBench(I_UserMoeaBench):
                 name_benchmark = self.problem.__class__.__name__
             
             return execute.MOEA_execute(self.result,self.problem,name_moea,name_benchmark)
-       # except Exception as e:
-            #print(e)
+        except Exception as e:
+            print(e)
 
 
     def hypervolume(self, N = None):

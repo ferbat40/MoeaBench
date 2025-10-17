2bff2b7479da535f5246a102c586f0dea26bd6a1
Fernando <setec.solda@gmail.com>
2025-10-17 16:51:45 -0300

    sdf

--- MoeaBench/plot_gen_result.py
+++ MoeaBench/plot_gen_result.py
@@ -19,7 +19,7 @@ class plot_gen_result(analyse):
              self.figure=go.Figure()
              
          #for pts in self.gen_moea:
-             for metric, gen in zip(self.generation,self.experiments):
+             for gen,lbl,metric in zip(self.generation,self.experiments,self.gen_moea):
      
                     gen=np.array(gen)
                     metric=np.array(metric)

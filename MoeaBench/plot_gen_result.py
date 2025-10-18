4fcb26d38a75fb50b712f8f53a03ad7ca5d502d9
Fernando <setec.solda@gmail.com>
2025-10-17 21:44:30 -0300

    sdf

--- MoeaBench/plot_gen_result.py
+++ MoeaBench/plot_gen_result.py
@@ -42,7 +42,7 @@ class plot_gen_result(analyse):
          
          self.figure.update_layout(       
                      xaxis=dict(title=self.metric[1], showgrid=True, gridcolor="#C3BDBD"),
-                     yaxis=dict(title=self.metric[0], showgrid=True, gridcolor="#C3BDBD"),
+                     yaxis=dict(title=self.metric[0], showgrid=True, gridcolor="#C3BDBD",tickformat =".0f"),
                      margin=dict(l=70,r=150,b=80,t=140),
                      plot_bgcolor="#FAFAFA",
                      paper_bgcolor="white",
@@ -61,7 +61,6 @@ class plot_gen_result(analyse):
                                      xanchor='left',
                                      yanchor='middle',
                                      font=dict(size=11)))
-         self.figure.update_yaxes(tickformat =".0f")
          self.PLT()
       
 

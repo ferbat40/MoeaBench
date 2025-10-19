0a44431af96b87c4864ef2dd690ea6420962a2e8
Fernando <setec.solda@gmail.com>
2025-10-18 23:28:50 -0300

    sdf

--- MoeaBench/analyse_others_metric_gen.py
+++ MoeaBench/analyse_others_metric_gen.py
@@ -7,13 +7,16 @@ class analyse_others_metric_gen(plot_gen):
     @staticmethod
     def IPL_plot_3D(*args, experiments, generations , objective, mtc, stead = False, mean = False ):  
       try: 
+        others = [i for i in [stead , mean]]
+        if not others == 1:
+           raise("only one metric should be chosen")
         data  = [b[0] for i in args for b in i.result.get_elements()]
         analyse_others_metric_gen.allowed_gen(generations)
         analyse_others_metric_gen.allowed_gen_max(data, mtc, generations[1])
         analyse_others_metric_gen.allowed_obj(data, objective)
         gen_moea=analyse_others_metric_gen.normalize_gen(data,generations,mtc,objective)
         evaluate = [np.arange(generations[0],generations[1]+1) for _ in range(len(data))] 
-        vet_aux = np.array([[np.nanstd(b) for b in i] for i in gen_moea])
+        vet_aux = np.array([[np.nanstd(b) if stead is True else np.nanmean(b) for b in i] for i in gen_moea])
         metric = [vet_aux[:,i:idx].flatten() for idx, i in enumerate(range(0,vet_aux.shape[1]), start = 1)]
         plot_g = analyse_others_metric_gen([evaluate,metric],experiments,"",  metric = ['objective mean','Generations']) 
         plot_g.configure()   

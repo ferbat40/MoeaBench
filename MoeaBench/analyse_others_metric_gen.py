8d9b168c1da673e579fc654c27dbee2a15fffd61
Fernando <setec.solda@gmail.com>
2025-10-18 22:54:33 -0300

    sdf

--- MoeaBench/analyse_others_metric_gen.py
+++ MoeaBench/analyse_others_metric_gen.py
@@ -12,12 +12,12 @@ class analyse_others_metric_gen(plot_gen):
         analyse_others_metric_gen.allowed_gen_max(data, mtc, generations[1])
         analyse_others_metric_gen.allowed_obj(data, objective)
         gen_moea=analyse_others_metric_gen.normalize_gen(data,generations,mtc,objective)
-        evaluate = [np.arange(generations[0],generations[1]) for _ in range(len(data))] 
+        evaluate = [np.arange(generations[0],generations[1]+1) for _ in range(len(data))] 
         vet_aux = np.array([[np.mean(b) for b in i] for i in gen_moea])
         metric = [vet_aux[:,i:idx].flatten() for idx, i in enumerate(range(0,vet_aux.shape[1]), start = 1)]
         plot_g = analyse_others_metric_gen([evaluate,metric],experiments,"",  metric = ['objective mean','Generations'])
-        #for pt, gen in zip(metric,evaluate):
-           #print(pt.shape,gen.shape)
+        for pt, gen in zip(metric,evaluate):
+           print(pt,gen)
         
         
         plot_g.configure()   

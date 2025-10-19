7bd02f600005893acfb6a7309b484531c3d6af78
Fernando <setec.solda@gmail.com>
2025-10-19 18:40:43 -0300

    sdf

--- MoeaBench/result_obj.py
+++ MoeaBench/result_obj.py
@@ -5,10 +5,11 @@ class result_obj(RESULT):
 
 
     def DATA(self,result, I,  N, mtc):
-        return [[idx,obj[:,I-1:I]] for idx, obj in enumerate(result.get_elements()[0][0].get_METRIC_gen().get_arr_Metric_gen()[mtc][0:N])]
+        return [[idx,obj[:,I-1:I]] for idx, obj in enumerate(result.get_elements()[0][0].get_METRIC_gen().get_arr_Metric_gen()[mtc][N[0]:N[1]])]
      
 
     def IPL_objectives(self, result, I,  N, mtc = 7):
+        self.allowed_gen(N)
         self.allowed_obj(result, mtc, I)
         self.allowed_gen_max(result, mtc, N)
         return self.DATA(result, I,  N, mtc)
--- MoeaBench/result_var.py
+++ MoeaBench/result_var.py
@@ -9,6 +9,7 @@ class result_var(result_obj):
     
 
       def IPL_variables(self, result, I,  N, mtc = 8):
+        self.allowed_gen(N)
         self.allowed_obj(result, mtc, I)
         self.allowed_gen_max(result, mtc, N)
         return self.DATA(result, I,  N, mtc)

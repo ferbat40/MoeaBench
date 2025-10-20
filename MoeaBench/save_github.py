cb52218c8529a921d24813d8eb970722e8789a21
Fernando <setec.solda@gmail.com>
2025-10-19 23:33:01 -0300

    xfs

--- MoeaBench/save_github.py
+++ MoeaBench/save_github.py
@@ -31,11 +31,11 @@ class save_github:
         with open(file_moea,'r') as f:
             code_moea = f.read()
         path_repository_moea =  f'user_moea/{file_moea}'        
-        repository.create_file(path_repository_moea,'add file',save_github(code_moea,"Moea.register_moea()"))
+        repository.create_file(path_repository_moea,'add file',save_github.data(code_moea,"Moea.register_moea()"))
 
 
         file_benchmark = f'{obj.pof.__class__.__name__}.py'
         with open(file_benchmark ,'r') as f:
             code_benchmark  = f.read()
         path_repository_benchmark =  f'user_benchmark/{file_benchmark}'        
-        repository.create_file(path_repository_benchmark ,'add file',save_github(code_benchmark,"benchmark.register_benchmark()" ))
+        repository.create_file(path_repository_benchmark ,'add file',save_github.data(code_benchmark,"benchmark.register_benchmark()" ))

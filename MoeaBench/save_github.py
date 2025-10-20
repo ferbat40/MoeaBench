3b5d42811869cad1ec53d75a8db6d59a4cae6ff5
Fernando <setec.solda@gmail.com>
2025-10-19 23:10:58 -0300

    xfs

--- MoeaBench/save_github.py
+++ MoeaBench/save_github.py
@@ -1,7 +1,19 @@
 from github import Github
 
 class save_github:
+
+    @staticmethod
+    def data(string,exlude):
+        string_class = string.splitlines() 
+        string_class_full=[]
+        for row in string_class:
+            if exlude in row:
+                continue
+            string_class_full.append(row)
+        print(string_class_full)
+        return string_class_full
     
+
     @staticmethod
     def save(obj):
         moea = obj.Moea.get_moea()
@@ -12,14 +24,18 @@ class save_github:
             pass
         token = Github("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")
         repository = token.get_user().get_repo('MoeaBench')
+
+        
         
         file_moea = f'{instance_moea.__class__.__name__}.py'
         with open(file_moea,'r') as f:
             code_moea = f.read()
-        repository.create_file(file_moea,'add file',code_moea)
+        path_repository_moea =  f'user_moea/{file_moea}'        
+        repository.create_file(path_repository_moea,'add file',save_github(code_moea,"Moea.register_moea()"))
 
 
         file_benchmark = f'{obj.pof.__class__.__name__}.py'
         with open(file_benchmark ,'r') as f:
             code_benchmark  = f.read()
-        repository.create_file(file_benchmark ,'add file',code_benchmark )
+        path_repository_benchmark =  f'user_benchmark/{file_benchmark}'        
+        repository.create_file(path_repository_benchmark ,'add file',save_github(code_benchmark,"benchmark.register_benchmark()" ))

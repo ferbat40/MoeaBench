f8b3469fbb4f14ab5ef75fc74348f0b49c8381e4
Fernando <setec.solda@gmail.com>
2025-10-23 10:03:23 -0300

    sdf

--- MoeaBench/analyse_others_metric_gen.py
+++ MoeaBench/analyse_others_metric_gen.py
@@ -1,29 +1,27 @@
 from .plot_gen import plot_gen
 import numpy as np
 from .E_metric import E_metric
-import warnings
-warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 class analyse_others_metric_gen(plot_gen):
      
     @staticmethod
     def std(value):
-      return np.nanstd(value) 
+      return np.nan if np.all(np.isnan(value)) else np.nanstd(value) 
 
     
     @staticmethod
     def mean(value):  
-      return  np.nanmean(value)
+      return np.nan if np.all(np.isnan(value)) else np.nanmean(value)
 
     
     @staticmethod
     def min(value):
-      return  np.nanmin(value) 
+      return np.nan if np.all(np.isnan(value)) else np.nanmin(value) 
 
 
     @staticmethod
     def max(value):
-      return np.nanmax(value) 
+      return np.nan if np.all(np.isnan(value)) else np.nanmax(value) 
 
       
     @staticmethod
@@ -49,18 +47,23 @@ class analyse_others_metric_gen(plot_gen):
     @staticmethod
     def IPL_plot_3D(*args, experiments, generations , objective, mtc, val_metric, types ):  
       label=[]
+      metric=[]
       data  = [b[0] for i in args for b in i.result.get_elements()]   
       analyse_others_metric_gen.allowed_gen(generations)
       analyse_others_metric_gen.allowed_gen_max(data, mtc, generations[1])
       gen_moea=analyse_others_metric_gen.normalize_gen(data,generations,mtc,objective)
       evaluate = [np.arange(generations[0],generations[1]+1) for _ in range(len(data))] 
-      metric = np.array([[ analyse_others_metric_gen.dict_metric()[val_metric[0]](np.array(line[i])) for i in [0,len(evaluate)-1]] for line in gen_moea])
+      
+      for i in [0,len(evaluate)-1]:
+        metric.append(np.array([[ analyse_others_metric_gen.dict_metric()[val_metric[0]](np.array(line[i])) for i in [i]] for line in gen_moea]))
+      
       name_metric =analyse_others_metric_gen.dict_metric_label()[val_metric[0]]
       label.append(name_metric.name)
       label.append('Generations')
       label.append(types)
       plot_g = analyse_others_metric_gen([evaluate,metric],experiments, metric = label) 
       plot_g.configure() 
+
      
 
   

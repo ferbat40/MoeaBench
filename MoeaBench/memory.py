1c0f5d59eaa5af0cd154de08e2548204789318d5
Fernando <setec.solda@gmail.com>
2025-10-26 23:00:04 -0300

    sdf

--- MoeaBench/memory.py
+++ MoeaBench/memory.py
@@ -2,11 +2,6 @@ from .DATA_arr import DATA_arr
 from .BENCH_conf import BENCH_conf
 from .DATA_conf import DATA_conf
 from .DATA_arr import DATA_arr
-from .GEN_hypervolume import GEN_hypervolume
-from .GEN_gd import GEN_gd
-from .GEN_gdplus import GEN_gdplus
-from .GEN_igd import GEN_igd
-from .GEN_igdplus import GEN_igdplus
 from .I_memory import I_memory
 
 

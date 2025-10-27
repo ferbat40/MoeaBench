796697c2705687e4ab3c8837fc3f349ee2606d37
Fernando <setec.solda@gmail.com>
2025-10-27 12:11:27 -0300

    sdf

--- MoeaBench/result_metric.py
+++ MoeaBench/result_metric.py
@@ -1,8 +1,30 @@
-from .RESULT import RESULT
+from .result_obj_var import result_obj_var
 import numpy as np
 
 
-class result_metric(RESULT):
+class result_metric(result_obj_var):
+
+    def DATA(self,result,generation, objective):
+        gen_f_test = [b[0].get_F_GEN() for b in result.get_elements()]
+        gen_f_max = max([len(gen)  for gen in gen_f_test])
+        generations = [0,gen_f_max] if isinstance(generation, (list)) and len(generation) == 0 else generation
+        result_metric.allowed_gen(generations)
+        result_metric.allowed_gen_max(gen_f_max,generations[1])
+        objectives = [1,2,3] if isinstance(objective, (list)) and  len(objective) == 0 else objective  
+        result_metric.allowed_obj(objectives)
+             
+        gen_f_valid = [b[0].get_F_GEN()[generations[0]:generations[1]] for b in result.get_elements()]
+        slicing = [[i-1,i]  for i in objectives]
+        F_gen = []
+        for i in range(len(gen_f_valid)):
+            vet_aux = []
+            for z in range(len(gen_f_valid[i])):
+                vet_aux.append(result_metric.slicing_arr(slicing,gen_f_valid[i][z]))
+            F_gen.append(vet_aux)           
+        F = [b[0].get_arr_DATA() for b in result.get_elements()]
+        F_slice = [np.hstack( [b[:,i:j]  for i,j in slicing]) for b in F ]        
+        return F_gen,F_slice 
+    
 
     def IPL_hypervolume(self, result, generation, objective):
         F_GEN, F =  self.DATA(result,generation, objective)
--- MoeaBench/result_obj_var.py
+++ MoeaBench/result_obj_var.py
@@ -1,7 +1,7 @@
-from .RESULT import RESULT
-import numpy as np
+from .IPL_MoeaBench import IPL_MoeaBench 
 
-class result_obj_var(RESULT):
+
+class result_obj_var(IPL_MoeaBench ):
 
     def F(self,result, generations = None):
         return [b[0].get_F_GEN() for b in result.get_elements()] if generations is None else [b[0].get_F_GEN()[generations[0]:generations[1]] for b in result.get_elements()]
@@ -19,14 +19,14 @@ class result_obj_var(RESULT):
         gen_f_test = self.dict_data()[default](result)
         gen_f_max = max([len(gen)  for gen in gen_f_test])
         generations = [0,gen_f_max] if isinstance(generation, (list)) and len(generation) == 0 else generation
-        RESULT.allowed_gen(generations)
-        RESULT.allowed_gen_max(gen_f_max,generations[1])        
+        result_obj_var.allowed_gen(generations)
+        result_obj_var.allowed_gen_max(gen_f_max,generations[1])        
         gen_f_valid = self.dict_data()[default](result,generations)
         slicing = [[objective-1,objective]]
         F_gen = []
         for i in range(len(gen_f_valid)):
             vet_aux = []
             for z in range(len(gen_f_valid[i])):
-                vet_aux.append(RESULT.slicing_arr(slicing,gen_f_valid[i][z]))
+                vet_aux.append(result_obj_var.slicing_arr(slicing,gen_f_valid[i][z]))
             F_gen.append(vet_aux)                   
         return F_gen 

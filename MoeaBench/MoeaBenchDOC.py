7de2d6ff3d86f0d41dc18389d7cc89cb158e1e2d
moeabench-rgb <moeabench@gmail.com>
2025-11-03 18:48:08 -0300

    df

---
+++ MoeaBench/MoeaBenchDOC.py
@@ -0,0 +1,19 @@
+from .MoeaBench import MoeaBench
+
+class MoeaBenchDOC(MoeaBench):
+    """
+        - Description:   
+            MoeaBench is a framework for experimentation, analysis, and 
+            development of benchmark problems for validating the performance     
+            of genetic algorithms.
+             
+        - Example :
+            experiment = moeabench()
+           
+        - NOTES:
+            - For more information about the framework, please visit the link:
+              https://moeabench-rgb.github.io/MoeaBench/
+    """
+    pass
+
+moeabench = MoeaBenchDOC
--- MoeaBench/factory.py
+++ MoeaBench/factory.py
@@ -1,31 +1,15 @@
 from .MoeaBench import MoeaBench
 
 class _MoeaBenchWrapper:
-    
-    def __init__(self):
-        self.__doc__ =   """
-        - Description:   
-            MoeaBench is a framework for experimentation, analysis, and 
-            development of benchmark problems for validating the performance     
-            of genetic algorithms.
-             
-        - Example :
-            experiment = moeabench()
-           
-        - NOTES:
-            - For more information about the framework, please visit the link:
-              https://moeabench-rgb.github.io/MoeaBench/
-    """
-    
 
+    
     def __getattr__(self, name):
         inst = MoeaBench()
         return getattr(inst, name)
 
 
     def __call__(self, *args, **kwargs):
-        obj = MoeaBench(*args,  **kwargs)
-        obj.__doc__ = self.__doc__
-        return obj
+        return MoeaBench(*args,  **kwargs)
+
 
 moeabench = _MoeaBenchWrapper()

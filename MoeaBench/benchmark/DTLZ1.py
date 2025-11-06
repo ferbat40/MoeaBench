b5055d5526bf2c7de4e5c86f5d01c000b47b6985
moeabench-rgb <moeabench@gmail.com>
2025-11-06 15:49:01 -0300

    df

--- MoeaBench/benchmark/DTLZ1.py
+++ MoeaBench/benchmark/DTLZ1.py
@@ -8,10 +8,11 @@ def DTLZ1(M = 3, K = 5, P = 700):
           - The benchmark problem should be used with an experiment, with the following syntax.
           experiment.problem = moeabench.benchmark.DTLZ1(args) 
         - Notes:
-          - [DTLZ1](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) for detailed information about the problem
-          - ([args](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/#arguments)) for detailed information on the parameters that can be passed for custom 
+          Click on the links below to obtain:
+          - [DTLZ1](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) detailed information about the problem
+          - ([args](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/#arguments)) detailed information on the parameters that can be passed for custom 
           configuration of the benchmark problem, see the parameters that can be passed. 
-          - [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) for information on possible error types
+          - [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) information on possible error types
         """
         problem = problems(DTLZ1.__name__)
         bk = problem.get_problem(M, K, P)

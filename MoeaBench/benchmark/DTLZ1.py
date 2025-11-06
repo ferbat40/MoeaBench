60eed99d6193b7edf197b7cffc0318164ac050fd
moeabench-rgb <moeabench@gmail.com>
2025-11-06 17:51:52 -0300

    sfe

--- MoeaBench/benchmark/DTLZ1.py
+++ MoeaBench/benchmark/DTLZ1.py
@@ -4,12 +4,12 @@ from .problems import problems
 @staticmethod
 def DTLZ1(M = 3, K = 5, P = 700):
         """
-        - DTLZ1:
-          - sinxtase:
-          experiment.problem = moeabench.benchmark.DTLZ1(args) 
-          - [DTLZ1](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) detailed information about the problem
-          - ([args](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/#arguments)) custom and default settings problem
-          - [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) information on possible error types
+         - DTLZ1:
+                      - sinxtase:
+                      experiment.problem = moeabench.benchmark.DTLZ1(args) 
+                      - [DTLZ1](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) detailed information about the problem
+                      - ([args](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/#arguments)) custom and default settings problem
+                      - [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) information on possible error types
         """
         problem = problems(DTLZ1.__name__)
         bk = problem.get_problem(M, K, P)

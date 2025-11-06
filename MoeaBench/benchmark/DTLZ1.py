a4d64a3573a82a5f4e42c49767ae6c498748ab56
moeabench-rgb <moeabench@gmail.com>
2025-11-06 17:55:42 -0300

    sfe

--- MoeaBench/benchmark/DTLZ1.py
+++ MoeaBench/benchmark/DTLZ1.py
@@ -8,8 +8,11 @@ def DTLZ1(M = 3, K = 5, P = 700):
          - DTLZ1:
                       - sinxtase:
                       experiment.problem = moeabench.benchmark.DTLZ1(args) 
+                      
                       - [DTLZ1](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) detailed information about the problem
+                      
                       - ([args](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/#arguments)) custom and default settings problem
+                      
                       - [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) information on possible error types
         ...
         """

5c4321a69a18f771537ef66153a6ff2520ef1592
moeabench-rgb <moeabench@gmail.com>
2025-11-06 14:10:27 -0300

    df

--- MoeaBench/benchmark/__init__.py
+++ MoeaBench/benchmark/__init__.py
@@ -16,10 +16,14 @@
             O valor de M deve ser M > 2.
             O valor de K deve ser K > 0.
         
-        - NOTES:
-          - Para informações sobre o método DTLZ1:
-          https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/
-          [moeabench.benchmark.DTLZ3](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/).
+        - Benchmarks:
+          experiment.problem = [moeabench.benchmark.DTLZ1](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/)
+          experiment.problem = [moeabench.benchmark.DTLZ2](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ2/DTLZ2/)
+          experiment.problem = [moeabench.benchmark.DTLZ3](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ3/DTLZ3/)
+          experiment.problem = [moeabench.benchmark.DTLZ4](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ4/DTLZ4/)
+          experiment.problem = [moeabench.benchmark.DTLZ5](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ5/DTLZ5/)
+          experiment.problem = [moeabench.benchmark.DTLZ6](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ6/DTLZ6/)
+          experiment.problem = [moeabench.benchmark.DTLZ7](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ7/DTLZ7/)
           
 """
 import os, importlib

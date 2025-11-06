2eba06b4a596d53c122d62fefe090a195a62c22a
moeabench-rgb <moeabench@gmail.com>
2025-11-06 16:52:30 -0300

    df

--- MoeaBench/benchmark/__init__.py
+++ MoeaBench/benchmark/__init__.py
@@ -3,13 +3,13 @@
             MoeaBench has implementations of several benchmark problems. 
             Click on the link for the respective benchmark problem of each experiment to 
             obtain more information about the problem and Pareto optimal front simulation.
-            - DTLZ1:
-              - sinxtase:
-              experiment.problem = moeabench.benchmark.DTLZ1(args) 
-              - [DTLZ1](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) detailed information about the problem
-              - ([args](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/#arguments)) detailed information on the parameters that can be passed for custom 
-              configuration of the benchmark problem, see the parameters that can be passed. 
-              - [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) information on possible error types
+               - DTLZ1:
+                   - sinxtase:
+                   experiment.problem = moeabench.benchmark.DTLZ1(args) 
+                   - [DTLZ1](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) detailed information about the problem
+                   - ([args](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/#arguments)) detailed information on the parameters that can be passed for custom 
+                   configuration of the benchmark problem, see the parameters that can be passed. 
+                   - [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) information on possible error types
           
 """
 import os, importlib

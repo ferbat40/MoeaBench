e351e7db396c9275ab8c88a2ea7a59d957ce0b1c
moeabench-rgb <moeabench@gmail.com>
2025-11-14 14:15:13 -0300

    sdf

--- MoeaBench/benchmark/DTLZ1.py
+++ MoeaBench/benchmark/DTLZ1.py
@@ -3,18 +3,7 @@ from .problems import problems
 
 @staticmethod
 def DTLZ1(M = 3, K = 5, P = 700):
-        """
-        - benchmark problem:
-        Click on the links for more
-        ...
-                - DTLZ1:
-                      - sinxtase:
-                      experiment.problem = moeabench.benchmark.DTLZ1(args) 
-                      - [DTLZ1](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) detailed information about the problem
-                      - ([args](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/#arguments)) custom and default settings problem
-                      - [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ1/DTLZ1/) information on possible error types
-        
-        """
+
         try:
             problem = problems(DTLZ1.__name__)
             bk = problem.get_problem(M, K, P)

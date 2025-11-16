9ba0c68c0a7552ff37bdf96218694c4383cc26f4
moeabench-rgb <moeabench@gmail.com>
2025-11-16 16:37:26 -0300

    dfs

--- MoeaBench/factory.py
+++ MoeaBench/factory.py
@@ -14,6 +14,32 @@ class _MoeaBenchWrapper:
             - For more information about the framework, please visit the link:
               https://moeabench-rgb.github.io/MoeaBench/
     """
+    """     
+        - Description:        
+            MoeaBench is a framework for experimentation, analysis, and 
+            development of benchmark problems for validating the performance     
+            of genetic algorithms.
+               ...
+               - genetic algorithms:
+                      - sinxtase:
+                      moeabench.MOEA
+                      - [NSGA-III](https://moeabench-rgb.github.io/MoeaBench/algorithms/NSGA3/) information about the genetic algorithm
+                      - [U-NSGA-3](https://moeabench-rgb.github.io/MoeaBench/algorithms/UNSGA3/) information about the genetic algorithm
+ 
+                ... 
+                 - benchmark problems:
+                      - sinxtase:
+                      moeabench.benchmark
+                      - [NSGA-III](https://moeabench-rgb.github.io/MoeaBench/algorithms/NSGA3/) information about the genetic algorithm
+                      - [U-NSGA-3](https://moeabench-rgb.github.io/MoeaBench/algorithms/UNSGA3/) information about the genetic algorithm
+                ...
+              
+              
+"""
+
+
+
+
 
     
     def __getattr__(self, name):

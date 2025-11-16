d977aeb23ea69be865092111326fca1b4023e977
moeabench-rgb <moeabench@gmail.com>
2025-11-16 16:40:41 -0300

    sf

--- MoeaBench/factory.py
+++ MoeaBench/factory.py
@@ -1,19 +1,6 @@
 from .MoeaBench import MoeaBench
 
 class _MoeaBenchWrapper:
-    """
-        - Description:   
-            MoeaBench is a framework for experimentation, analysis, and 
-            development of benchmark problems for validating the performance     
-            of genetic algorithms.
-             
-        - Example :
-            experiment = moeabench()
-           
-        - NOTES:
-            - For more information about the framework, please visit the link:
-              https://moeabench-rgb.github.io/MoeaBench/
-    """
     """     
         - Description:        
             MoeaBench is a framework for experimentation, analysis, and 

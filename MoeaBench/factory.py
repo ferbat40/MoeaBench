9f6a1acd3de0d1f90cbcc706a15232e955406b31
moeabench-rgb <moeabench@gmail.com>
2025-11-16 17:07:59 -0300

    sf

--- MoeaBench/factory.py
+++ MoeaBench/factory.py
@@ -2,43 +2,16 @@ from .MoeaBench import MoeaBench
 
 class _MoeaBenchWrapper:
     """
-- Benchmarks:
-    MoeaBench uses several evolutionary algorithms from the pymoo library.
-    ...
-    - NSGA-III:
-        - sintaxe:
-          experiment.moea = moeabench.MOEA.NSGA_III(args)
-        - [NSGA-III](https://moeabench-rgb.github.io/MoeaBench/algorithms/NSGA3/) information about the genetic algorithm
-    ...
-    - U-NSGA-III:
-        - sintaxe:
-          experiment.moea = moeabench.MOEA.U_NSGA_III(args)
-        - [U-NSGA-3](https://moeabench-rgb.github.io/MoeaBench/algorithms/UNSGA3/) information about the genetic algorithm
-    ...
-    - SPEA-II:
-        - sintaxe:
-          experiment.moea = moeabench.MOEA.SPEA_II(args)
-        - [SPEA-II](https://moeabench-rgb.github.io/MoeaBench/algorithms/SPEA2/) information about the genetic algorithm
-    ...
-    - MOEA/D:
-        - sintaxe:
-          experiment.moea = moeabench.MOEA.MOEAD(args)
-        - [MOEA/D](https://moeabench-rgb.github.io/MoeaBench/algorithms/MOEAD/) information about the genetic algorithm
-    ...
-    - RVEA:
-        - sintaxe:
-          experiment.moea = moeabench.MOEA.RVEA(args)
-        - [RVEA](https://moeabench-rgb.github.io/MoeaBench/algorithms/RVEA/) information about the genetic algorithm
-    ...
-    - my_new_moea:
-        - sintaxe:
-          experiment.moea = moeabench.MOEA.my_new_moea(args)
-        - [my_new_moea](https://moeabench-rgb.github.io/MoeaBench/implement_moea/memory/memory/) information about the method
-    ...
-"""
-
-
-
+        - Description:   
+            MoeaBench is a framework for experimentation, analysis, and 
+            development of benchmark problems for validating the performance     
+            of genetic algorithms.
+             
+        - how to start
+            experiment = moeabench()
+            Click the link for a step-by-step guide:
+            https://moeabench-rgb.github.io/MoeaBench/
+    """
 
     
     def __getattr__(self, name):

a5053c10cf92a1704febd8f6dfe8a453b46cb634
moeabench-rgb <moeabench@gmail.com>
2025-11-16 16:53:04 -0300

    sf

--- MoeaBench/factory.py
+++ MoeaBench/factory.py
@@ -1,41 +1,41 @@
 from .MoeaBench import MoeaBench
 
 class _MoeaBenchWrapper:
-    """     
-        - Benchmarks:       
-            MoeaBench uses several evolutionary algorithms from the pymoo library.
-               ...
-               - NSGA-III:
-                      - sinxtase:
-                      experiment.moea = moeabench.MOEA.NSGA_III(args) 
-                      - [NSGA-III](https://moeabench-rgb.github.io/MoeaBench/algorithms/NSGA3/) information about the genetic algorithm
-               ... 
-               - U-NSGA-III:
-                      - sinxtase:
-                      experiment.moea = moeabench.MOEA.U_NSGA_III(args)
-                      - [U-NSGA-3](https://moeabench-rgb.github.io/MoeaBench/algorithms/UNSGA3/) information about the genetic algorithm
-                ...
-               - SPEA-II:
-                      - sinxtase:
-                      experiment.moea = moeabench.MOEA.SPEA_II(args) 
-                      - [SPEA-II](https://moeabench-rgb.github.io/MoeaBench/algorithms/SPEA2/) information about the genetic algorithm
-               ... 
-               - MOEA/D:
-                      - sinxtase:
-                      experiment.moea = moeabench.MOEA.MOEAD(args) 
-                      - [MOEA/D](https://moeabench-rgb.github.io/MoeaBench/algorithms/MOEAD/) information about the genetic algorithm
-                 ... 
-               - RVEA:
-                      - sinxtase:
-                      experiment.moea = moeabench.MOEA.RVEA(args) 
-                      - [RVEA](https://moeabench-rgb.github.io/MoeaBench/algorithms/RVEA/) information about the genetic algorithm
-                 ...       
-               - my_new_moea:      
-                      - sinxtase:
-                      experiment.problem = experiment.MOEA.my_new_moea(args)       
-                      - [my_new_moea](https://moeabench-rgb.github.io/MoeaBench/implement_moea/memory/memory/) information about the method, 
-                 ...          
     """
+- Benchmarks:
+    MoeaBench uses several evolutionary algorithms from the pymoo library.
+    ...
+    - NSGA-III:
+        - sintaxe:
+          experiment.moea = moeabench.MOEA.NSGA_III(args)
+        - [NSGA-III](https://moeabench-rgb.github.io/MoeaBench/algorithms/NSGA3/) information about the genetic algorithm
+    ...
+    - U-NSGA-III:
+        - sintaxe:
+          experiment.moea = moeabench.MOEA.U_NSGA_III(args)
+        - [U-NSGA-3](https://moeabench-rgb.github.io/MoeaBench/algorithms/UNSGA3/) information about the genetic algorithm
+    ...
+    - SPEA-II:
+        - sintaxe:
+          experiment.moea = moeabench.MOEA.SPEA_II(args)
+        - [SPEA-II](https://moeabench-rgb.github.io/MoeaBench/algorithms/SPEA2/) information about the genetic algorithm
+    ...
+    - MOEA/D:
+        - sintaxe:
+          experiment.moea = moeabench.MOEA.MOEAD(args)
+        - [MOEA/D](https://moeabench-rgb.github.io/MoeaBench/algorithms/MOEAD/) information about the genetic algorithm
+    ...
+    - RVEA:
+        - sintaxe:
+          experiment.moea = moeabench.MOEA.RVEA(args)
+        - [RVEA](https://moeabench-rgb.github.io/MoeaBench/algorithms/RVEA/) information about the genetic algorithm
+    ...
+    - my_new_moea:
+        - sintaxe:
+          experiment.moea = moeabench.MOEA.my_new_moea(args)
+        - [my_new_moea](https://moeabench-rgb.github.io/MoeaBench/implement_moea/memory/memory/) information about the method
+    ...
+"""
 
 
 

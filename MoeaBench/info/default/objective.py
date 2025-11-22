741b99fd914af0a47ff8f8e89d0c36472bd4112f
moeabench-rgb <moeabench@gmail.com>
2025-11-22 20:08:43 -0300

    xv

--- MoeaBench/info/default/objective.py
+++ MoeaBench/info/default/objective.py
@@ -1,15 +1,17 @@
-class objective:
-      
+class objective:  
       """
         - array with objectives in generations:
-        default mode
-        ...
-                - experiment.objective():
-                      - arguments:
-                        - objective: returns the first objective  
-                        - generations: It returns the goal for all generations.
+        default mode:
+
+          - experiment.objective():
+             
+               - arguments:
+              
+                 - objective: returns the first objective  
+                 - generations: It returns the goal for all generations.
       """
 
+      
       def __repr__(self):
             return self.__class__.__doc__
       

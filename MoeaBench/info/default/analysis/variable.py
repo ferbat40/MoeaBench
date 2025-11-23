ebc034756c0932e501d9ff182f761c30d8ecb8c1
moeabench-rgb <moeabench@gmail.com>
2025-11-23 16:27:24 -0300

    dfs

--- MoeaBench/info/default/analysis/variable.py
+++ MoeaBench/info/default/analysis/variable.py
@@ -1,28 +1,8 @@
+from IPython.display import Markdown, display
+
 
 class variable:  
-      """
-      - array with variables in generations:
 
-        ● default mode: var = experiment.variable():
-             
-          ○ arguments:             
-            ● variable: returns the first objective  
-            ● generations: It returns the goal for all generations.
-
-                     
-        ● Notes:
-          
-          ○ selecting 'variable' in:
-            
-            ● var = experiment.variable()
-              ○ You can access more information about the method.
-
-          ○ for more information:
-            
-            ● [variable](https://moeabench-rgb.github.io/MoeaBench/analysis/variables/data/variable/) information about the method.
-            ● [Exception](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/exceptions/) information on possible error types. 
-            
-      """
 
 
      def __call__(self):

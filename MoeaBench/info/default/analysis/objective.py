1c8767741818e32e785d9f0b6299ddcd0e51d3ae
moeabench-rgb <moeabench@gmail.com>
2025-11-23 16:04:59 -0300

    dfs

--- MoeaBench/info/default/analysis/objective.py
+++ MoeaBench/info/default/analysis/objective.py
@@ -1,31 +1,34 @@
-class objective:  
-      """
-      - array with objectives in generations:
+from IPython.display import Markdown, display
 
-        ● default mode: obj = experiment.objective():
+class objective:  
+    
+    
+    def __call__(self):
+        display(Markdown("""
+- **array with objectives in generations:**
+
+- **default mode:**
+    - obj = experiment.objective():
              
-          ○ arguments:             
-            ● objective: returns the first objective  
-            ● generations: It returns the goal for all generations.
+- **arguments (default setting if no arguments are provided):**             
+    - objective: returns the first objective  
+    - generations: It returns the goal for all generations.
 
                      
-        ● Notes:
+- **Notes:**
           
-          ○ selecting 'objective' in:
-            
-            ● obj = experiment.objective()
-              ○ You can access more information about the method.
+    - selecting 'objective':        
+      - obj = experiment.objective()
+        - You can access more information about the method.
 
-          ○ for more information:
+    - for more information:
             
-            ● [objective](https://moeabench-rgb.github.io/MoeaBench/analysis/objectives/data/objective/) information about the method.
-            ● [Exception](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/exceptions/) information on possible error types. 
+      - [objective](https://moeabench-rgb.github.io/MoeaBench/analysis/objectives/data/objective/) 
+        - information about the method.
+      - [Exception](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/exceptions/) 
+        - information on possible error types. 
             
-      """
-
-
-      def __repr__(self):
-            return self.__class__.__doc__
+"""))
       
           
      

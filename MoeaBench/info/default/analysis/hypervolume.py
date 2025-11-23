d006c9cf44d31851d9fd651bfa958841bbb26b73
moeabench-rgb <moeabench@gmail.com>
2025-11-23 11:46:00 -0300

    dfs

--- MoeaBench/info/default/analysis/hypervolume.py
+++ MoeaBench/info/default/analysis/hypervolume.py
@@ -2,54 +2,20 @@ from IPython.display import Markdown, display
 
 
 class hypervolume:  
-      """
-      - array containing the hypervolume metric calculation:
-
-      - default mode: hv = experiment.hypervolume()
-             
-        ○ arguments (default setting if no arguments are provided):     
-              
-          ● objective = [1,2...N]: calculates the hypervolume metric for all objectives. 
-          ● generations = [0,N]: calculates the hypervolume metric for all generations.
-
-      - Notes:
-          
-        ○ selecting 'hypervolume':
-            
-          ● hv = experiment.hypervolume()
-            ○ You can access more information about the method.
-
-        ○ for more information:
-            
-          ● [hypervolume](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/hypervolume/) 
-            - information about the method, examples and more.
-          ● [Exception](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/exceptions/) 
-            - information on possible error types.
-      """
+      
    
 
       def __call__(self):
         display(Markdown("""
-      - array containing the hypervolume metric calculation:
+- **Array containing the hypervolume metric calculation**
 
-      - default mode: hv = experiment.hypervolume()
-             
-        ○ arguments (default setting if no arguments are provided):     
-              
-          ● objective = [1,2...N]: calculates the hypervolume metric for all objectives. 
-          ● generations = [0,N]: calculates the hypervolume metric for all generations.
+- **Default mode:** `hv = experiment.hypervolume()`
 
-      - Notes:
-          
-        ○ selecting 'hypervolume':
-            
-          ● hv = experiment.hypervolume()
-            ○ You can access more information about the method.
+- **Arguments (default if none provided):**
+  - `objective = [1,2,...,N]`
+  - `generations = [0,N]`
 
-        ○ for more information:
-            
-          ● [hypervolume](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/hypervolume/) 
-            - information about the method, examples and more.
-          ● [Exception](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/exceptions/) 
-            - information on possible error types.
-      """))
+- **Notes:**
+  - [Hypervolume docs](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/hypervolume/)
+  - [Possible exceptions](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/exceptions/)
+"""))

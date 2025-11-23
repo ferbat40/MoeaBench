43076e59f32a496da9afb0e2692755739d72cac1
moeabench-rgb <moeabench@gmail.com>
2025-11-22 21:09:42 -0300

    xv

--- MoeaBench/info/default/DTLZ1.py
+++ MoeaBench/info/default/DTLZ1.py
@@ -22,7 +22,7 @@ class DTLZ1:
           ○ for more information:
             
             ● [implementation](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ/DTLZ1/DTLZ1/) detailed implementation information.
-            ● ([arguments](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ/arguments/)) custom and default settings problem. 
+            ● [arguments](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ/arguments/) custom and default settings problem. 
             ● [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ/exceptions/) information on possible error types
              
       """
--- MoeaBench/info/default/DTLZ2.py
+++ MoeaBench/info/default/DTLZ2.py
@@ -22,7 +22,7 @@ class DTLZ2:
           ○ for more information:
             
             ● [implementation](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ/DTLZ2/DTLZ2/) detailed implementation information.
-            ● ([arguments](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ/arguments/)) custom and default settings problem. 
+            ● [arguments](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ/arguments/) custom and default settings problem. 
             ● [Exception](https://moeabench-rgb.github.io/MoeaBench/problems/DTLZ/exceptions/) information on possible error types
             
       """

16a98e3197454a6fa99877640248f8b565899021
moeabench-rgb <moeabench@gmail.com>
2025-11-23 11:42:23 -0300

    dfs

--- MoeaBench/info/default/analysis/hypervolume.py
+++ MoeaBench/info/default/analysis/hypervolume.py
@@ -29,16 +29,4 @@ class hypervolume:
    
 
       def __call__(self):
-        display(Markdown("""
-- **Array containing the hypervolume metric calculation**
-
-- **Default mode:** `hv = experiment.hypervolume()`
-
-- **Arguments (default if none provided):**
-  - `objective = [1,2,...,N]`
-  - `generations = [0,N]`
-
-- **Notes:**
-  - [Hypervolume docs](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/hypervolume/)
-  - [Possible exceptions](https://moeabench-rgb.github.io/MoeaBench/analysis/metrics/data/exceptions/)
-"""))
+        display(Markdown(self.__class__.__doc__))

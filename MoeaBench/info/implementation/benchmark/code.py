02ad11603e7351c650587fb98f1f6770ec6b7726
moeabench-rgb <moeabench@gmail.com>
2025-11-24 12:50:09 -0300

    dfs

--- MoeaBench/info/implementation/benchmark/code.py
+++ MoeaBench/info/implementation/benchmark/code.py
@@ -5,36 +5,37 @@ class code:
       def __call__(self):
           display(Markdown("""
 - **User implementation for their own benchmark problem:**
+                           
+    Custom implementations can also be used with MoeaBench.
 
-  - **features**
+    - **General rules**
              
-    - Any benchmark problem can be implemented as long as it adheres to 
-    implementation rules:     
+        - Any benchmark problem can be implemented as long as it adheres to 
+        implementation rules.     
     
-    - possibilities of use:
+    - **Supported implementations**:
     
-      - Implemented in Google Colab cells to run in memory.
+        - Implemented in Google Colab cells to run in memory.
                            
-      - Implemented in a file and coupled to MoeaBench via the user's 
-      repository on GitHub.
+        - Implemented in a file and coupled to MoeaBench via the user's 
+        repository on GitHub.
+                        
+    - **User options with:**
+                           
+        - native implementations of MoeaBench algorithms.
+                           
+        - user-implemented algorithm. 
     
-    - proposed example:
+    - **Example**:
       
-      - Deb's DTLZ5 benchmark problem.
-    
-              
-    - A user-implemented problem can be used by: 
-                           
-      - native implementations of MoeaBench algorithms.
-                           
-      - user-implemented algorithm.
-       
+        - Deb's DTLZ5 benchmark problem.
+        
 
-  - **for more information access the links:**
+    - **For more information access the links:**
             
-    - [example code](https://moeabench-rgb.github.io/MoeaBench/implement_benchmark/example_code/example_code/) 
-      - Detailed information on the implementation process of the example mentioned.
+        - [example code](https://moeabench-rgb.github.io/MoeaBench/implement_benchmark/example_code/example_code/) 
+          - Detailed information on the implementation process of the example mentioned.
             
-    - [Exception](https://moeabench-rgb.github.io/MoeaBench/experiments/combinations/combinations/) 
-      - MoeaBench allows combinations for running algorithms with benchmark problems.
+        - [possible execution combinations](https://moeabench-rgb.github.io/MoeaBench/experiments/combinations/combinations/) 
+          - MoeaBench allows combinations for running algorithms with benchmark problems.
       """))

4b8e2ab31aeee98fedf8ebb2f01251011eee8213
moeabench-rgb <moeabench@gmail.com>
2025-11-24 14:04:41 -0300

    dfs

--- MoeaBench/info/default/benchmark/DPF1.py
+++ MoeaBench/info/default/benchmark/DPF1.py
@@ -12,7 +12,7 @@ class DPF1:
 
       - M = 3: number of problem objectives 
       - K = 5: number represents the size of the vector K related 
-      to the decision variables.
+        to the decision variables.
       - D = 2: number of essential objectives
       - P = 700: number of samples to the Pareto optimum.
 

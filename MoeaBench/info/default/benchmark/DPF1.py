2e4fee1da2eb96946eeca8bef40ebfb1d2d3cd8e
moeabench-rgb <moeabench@gmail.com>
2025-11-24 14:16:11 -0300

    dfs

--- MoeaBench/info/default/benchmark/DPF1.py
+++ MoeaBench/info/default/benchmark/DPF1.py
@@ -10,15 +10,15 @@ class DPF1:
              
     - arguments (default setting if no arguments are provided):      
 
-      - *M = 3*: number of problem objectives 
-      - *K = 5*: number represents the size of the vector K related   
+      - *M* = 3: number of problem objectives 
+      - *K* = 5: number represents the size of the vector K related   
         to the decision variables.
-      - *D = 2*: number of essential objectives
-      - *P = 700*: number of samples to the Pareto optimum.
+      - *D* = 2: number of essential objectives
+      - *P* = 700: number of samples to the Pareto optimum.
 
   - **Notes:**
           
-    - selecting 'DPF1':
+    - selectings *DPF1*:
             
       experiment.benchmark = experiment.benchmark.DPF1()
       - You can access more information about the method.

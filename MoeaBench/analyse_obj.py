de1c3370b3359818a8a7098771bf8b53fd17805b
moeabench-rgb <moeabench@gmail.com>
2025-11-28 17:52:46 -0300

    dfs

--- MoeaBench/analyse_obj.py
+++ MoeaBench/analyse_obj.py
@@ -6,7 +6,7 @@ class analyse_obj(plot_3D):
       
     @staticmethod
     def IPL_plot_3D(experiments, data, bench, array, objectives):
-        arr = [np.concatenate(   (array[i][2],array[i][2])) for i in range(0, len(array))]
+        arr = [np.concatenate(   (array[i][0],array[i][0])) for i in range(0, len(array))]
         #for i in arr:
             #print(i.shape)
             #join = np.concatenate(   (array[0][50],array[0][-1]))

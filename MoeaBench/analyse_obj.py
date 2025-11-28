87bec2fe514afe17660854bdc39b2526eb7e3d08
moeabench-rgb <moeabench@gmail.com>
2025-11-28 17:25:56 -0300

    dfs

--- MoeaBench/analyse_obj.py
+++ MoeaBench/analyse_obj.py
@@ -6,14 +6,19 @@ class analyse_obj(plot_3D):
       
     @staticmethod
     def IPL_plot_3D(experiments, data, bench, array, objectives):
-        arr = []
-        join = np.concatenate(   (array[0][50],array[0][-1]))
-        arr.append(join)
+        arr = [np.concatenate(   (array[0][50],array[0][-1])) for i in array]
+        #for i in arr:
+            #print(i.shape)
+            #join = np.concatenate(   (array[0][50],array[0][-1]))
+            #arr.extend(join.shape)
+        #arr = []
+        #join = np.concatenate(   (array[0][50],array[0][-1]))
+        #arr.append(join)
         try:
             analyse_obj.allowed_obj(bench,bench[0],experiments,objectives)
             axis =  [i for i in range(0,3)]    if len(objectives) == 0 else [i-1 if i > 0 else 0 for i in objectives] 
             #analyse_obj.allowed_DATA(data,experiments)
-            if not len([i for i in array[0][0] if len(i) == 0]) == 0:   
+            if not len([i for i in arr if len(i) == 0]) == 0:   
                 raise ValueError (f'No results found for plot')
             plot_3D_obj =  analyse_obj(bench,arr,experiments,axis)
             plot_3D_obj.configure()

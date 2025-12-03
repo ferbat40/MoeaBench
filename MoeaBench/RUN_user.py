61bee7f7255d412b2d206e1889e4603acd6a6dd1
moeabench-rgb <moeabench@gmail.com>
2025-12-03 14:55:56 -0300

    sfd

--- MoeaBench/RUN_user.py
+++ MoeaBench/RUN_user.py
@@ -3,7 +3,6 @@ from .runner import runner
 
 class RUN_user(runner):
 
-
      def MOEA_execute(self,result,problem,name_moea,name_benchmark):
           data = result.edit_DATA_conf().get_DATA_MOEA().evaluation()
           result.DATA_store(name_moea,

606caee14541c551c8565ce7886faaf62b04f135
moeabench-rgb <moeabench@gmail.com>
2025-12-23 15:32:42 -0300

    sdf

--- test4.py
+++ test4.py
@@ -6,30 +6,28 @@ import numpy as np
 os.system("cls")  
 
 
+#Stopped due to hypervolume
+import numpy as np
+def stop(experiment):
+    #restores the hypervolume of all generations to date.
+    metric = mb.hypervolume.trace(experiment)
+    hv = metric
+    #checks generations every 20
+    if len(hv)  % 20 == 0:
+        gen = np.diff(hv[-21:])
+        mean = np.mean(np.abs(gen))
+        std_gen = np.std(gen)
+        #If no increase is considered, execution is halted.
+        return mean < 1e-3 and std_gen < 1e-3
+
+
 exp = mb.experiment()
 exp.name = 'experiment 1'
 exp.benchmark = mb.benchmarks.DTLZ1()
-exp.moea = mb.moeas.MOEAD(generations = 10, population = 150)
+exp.moea = mb.moeas.MOEAD(generations = 10, population = 50)
 exp.moea.generations=150
 exp.moea.seed = 4
-exp.run()
+exp.stop = stop
+exp.run(repeat = 2)
 
 
-exp2 = mb.experiment()
-exp2.name = 'experiment 2'
-exp2.benchmark = mb.benchmarks.DTLZ1()
-exp2.moea = mb.moeas.NSGA3(generations = 10, population = 150)
-exp2.moea.generations=300
-exp2.run()
-
-ref = [exp, exp2]
-
-
-hv_100  = mb.hypervolume(exp, generation=100, reference = ref) 
-print(hv_100)
-
-
-ht = mb.hypervolume.trace(exp, reference = ref)
-print(ht)
-
-mb.hypervolume.timeplot(exp, exp2)

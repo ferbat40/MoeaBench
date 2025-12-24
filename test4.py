c34650a68eb996d4272bc107b846154db3ab6a45
moeabench-rgb <moeabench@gmail.com>
2025-12-24 20:12:51 -0300

    sdf

--- test4.py
+++ test4.py
@@ -6,19 +6,7 @@ import numpy as np
 os.system("cls")  
 
 
-#Stopped due to hypervolume
-import numpy as np
-def stop(experiment):
-    #restores the hypervolume of all generations to date.
-    metric = mb.hypervolume.trace(experiment)
-    hv = metric
-    #checks generations every 20
-    if len(hv)  % 20 == 0:
-        gen = np.diff(hv[-21:])
-        mean = np.mean(np.abs(gen))
-        std_gen = np.std(gen)
-        #If no increase is considered, execution is halted.
-        return mean < 1e-3 and std_gen < 1e-3
+
 
 
 exp = mb.experiment()
@@ -27,28 +15,26 @@ exp.benchmark = mb.benchmarks.DTLZ1()
 exp.moea = mb.moeas.MOEAD(generations = 10, population = 50)
 exp.moea.generations=150
 exp.moea.seed = 4
-exp.stop = stop
 exp.name = "turicer"
 #exp.run()
 
 
 exp2 = mb.experiment()
-#exp2.name = 'experiment 2'
 exp2.benchmark = mb.benchmarks.DTLZ1()
-exp2.moea = mb.moeas.NSGA3(generations = 10, population = 200)
-exp2.moea.generations=300
-exp2.run(repeat = 5)
+exp2.moea = mb.moeas.NSGA3(generations = 10, population = 100)
+exp2.moea.generations=200
+exp2.run(repeat = 3)
 
 
-F_GEN = exp2.round[0].dominated.variables
+nd = exp2.round[0].front
+pop = exp2.round[0].objectives
 
 
-print(F_GEN.shape )
-
-ref = [exp, exp2]
-#ht = mb.hypervolume.trace(exp, reference=ref)
-#mb.hypervolume.timeplot(exp2, exp, reference=ref)
-#mb.spaceplot (exp, exp2, exp.front(100)) 
+mask = np.ones(pop.shape[0], dtype=bool)
+print(    nd[:   , None, :])
 
 
-#mb.surfaceplot(exp, exp.objectives(generation = 10), exp2)
+#mask_nd = np.any(
+    #np.all(np.isclose(F[:, None, :], F_nd[None, :, :]), axis=2),
+   # axis=1
+#)

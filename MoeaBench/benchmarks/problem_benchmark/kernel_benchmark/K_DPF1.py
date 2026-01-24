9f53f63ad602023b39712de5ac17f507513724db
moeabench-rgb <moeabench@gmail.com>
2026-01-23 22:09:02 -0300

    xdf

--- MoeaBench/benchmarks/problem_benchmark/kernel_benchmark/K_DPF1.py
+++ MoeaBench/benchmarks/problem_benchmark/kernel_benchmark/K_DPF1.py
@@ -13,28 +13,22 @@ class K_DPF1(H_DPF):
 
 
     def Y1 (self,D,X,Gxr,idx):
-      # print("Y1")
        return 1/2*np.prod(X[:,0:D-1], axis = 1).reshape(X.shape[0],1)*(1+Gxr)
 
 
     def Y2 (self,D,X,Gxr,idx):
-       # print(X[:,0:D-idx-1],"    ",X[:,D-idx-1:D-idx] )
-       # print("Y2")
         return 1/2*np.prod(X[:,0:D-idx-1], axis = 1).reshape(X.shape[0],1)*(1-X[:,D-idx-1:D-idx])*(1+Gxr)
 
 
     def Yd (self,D,X,Gxr,idx):
-       # print("Y3")
         return 1/2*(1-X[:,0:1])*(1+Gxr)
     
 
     def FD1(self,F,vet_chaos,idx):
-       # print("Y4")
         return lambda row,D: np.dot(F,vet_chaos[row:row+D,:])
     
 
     def FM(self,F,vet_chaos,idx):
-        #print("Y5")
         return lambda row,D: np.dot(F,vet_chaos[row:row+D,:])
 
 
@@ -64,7 +58,6 @@ class K_DPF1(H_DPF):
 
 
     def calc_f(self,X,G):
-       # print(" X  ",X)
         D = self.CACHE.get_BENCH_CI().get_D()
         M = self.CACHE.get_BENCH_CI().get_M()
         vet_F_D = [self.calc_F_D(Fd,D) for Fd, i in enumerate(range(0,D), start = 1)]  
@@ -72,7 +65,6 @@ class K_DPF1(H_DPF):
         vet_chaos = self.calc_NU(1,(M-D)*D,(M-D)*D)
         vet_F_C = [self.calc_F_C(Fc,M-D) for Fc, i in enumerate(range(0,M-D), start = 1)]
         chaos = list(map(lambda Keys: self.param_CHAOS()[Keys[1]](F,vet_chaos,Keys[0]),enumerate(vet_F_C)))
-        print(vet_F_D,"  ",vet_F_C ,"  ",chaos)
         return self.calc_F_PD(chaos,F,D,vet_chaos)
      
 

b7cd1068af2efbce2de20158089ec2911433e677
moeabench-rgb <moeabench@gmail.com>
2026-01-24 16:50:47 -0300

    sfg

--- test4.py
+++ test4.py
@@ -8,19 +8,8 @@ os.system("cls")
 
 exp2 = mb.experiment()
 exp2.name = 'experiment 1'
-exp2.benchmark = mb.benchmarks.DPF4(M = 10, D = 9, K=10, P =2)
-#print(exp2.optimal.front().shape)
-#mb.spaceplot(exp2.optimal.front(), objectives = [1, 2, 3])
-
-
-#exp2.moea = mb.moeas.NSGA3(population=150, generations=300)
-#exp2.run()
-#print(exp2.benchmark.__class__.__name__.split('_')[1]," M =",exp2.benchmark.M," N =",exp2.optimal.set().shape[1])
-#exp2.benchmark.M = 5
-#exp2.benchmark.D = 4
-#print(exp2.benchmark.__class__.__name__.split('_')[1]," M =",exp2.benchmark.M," N =",exp2.optimal.set().shape[1])
-#exp2.benchmark.M = 10
-#exp2.benchmark.D = 9
-#print(exp2.benchmark.__class__.__name__.split('_')[1]," M =",exp2.benchmark.M," N =",exp2.optimal.set().shape[1])
-
-
+exp2.benchmark = mb.benchmarks.DTLZ5(M = 3, K = 10, P = 1000)
+benchmark = exp2.benchmark.__class__.__name__.split('_')[1]
+name = f"c:\\optimal\\legacy_{benchmark}_M_{exp2.benchmark.M}_K_{exp2.benchmark.K}_N_{exp2.optimal.set().shape[1]}_samples_{exp2.benchmark.P}.csv"
+print(name)
+np.savetxt(name, exp2.optimal.front(), delimiter=',', fmt='%f')
